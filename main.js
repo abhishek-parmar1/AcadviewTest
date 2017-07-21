3c752a379652bab321d28cbf1d168ab8f069299f
Abhishek <parmarabhishek.53@gmail.com>
2017-07-21 11:40:48 +0530

    user can add a todo

--- main.js
+++ main.js
@@ -2,5 +2,33 @@ var ngTodo = angular.module('ngTodo', []);
 
 ngTodo.controller('mainController',function($scope){
     
-    $scope.todoArray = ['Dress-up','Attend Class','Download Lecture','Complete Extra Project Objectives','Work on Self Project'];
+    $scope.todoArray = [
+        {
+            title : 'Get-up',
+            details : 'I must get-up early today.'
+        },
+        {
+            title : 'Attend Class',
+            details : 'I must Attend Class today.'
+        },
+        {
+            title : 'Download Lecture',
+            details : 'I must Download Lecture today.'
+        },
+        {
+            title : 'Complete Extra Project Objectives',
+            details : 'I must Complete Extra Project Objectives today.'
+        },
+        {
+            title : 'Work on Self Project',
+            details : 'I must Work on Self Project today.'
+        }
+                        ];
+    
+    $scope.addTodo = function(todaName,todoDetails){
+        $scope.todoArray[$scope.todoArray.length]={
+            title : todaName,
+            details : todoDetails
+        };
+    }
 });
